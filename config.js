28f945b09fb2a68c98ccbe2b2e864aa0774bc48e
xoo59568-art <xoo59568@gmail.com>
2026-07-03 22:41:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -42,5 +42,7 @@ export default {
   MONGODB_URI: process.env.MONGODB_URI || "",
   WORK_TYPE: process.env.WORK_TYPE || "public",
   STATUS_REACT: isTrue(process.env.STATUS_REACT) || false,
+  PAIR_SITE_URL: process.env.PAIR_SITE_URL || "https://web-pair-qr-5wcm.onrender.com",
+  PAIR_API_TOKEN: process.env.PAIR_API_TOKEN || "sreejan@900",
   AUTH_DIR,
 };
